f5a334b4d8693a126284135b6878e2c09f5a1ae9
Mohamed Muflahi <muflahimohamed88@gmail.com>
2022-11-05 19:50:31 -0400

    messages index

--- back-end/app/controllers/messages_controller.rb
+++ back-end/app/controllers/messages_controller.rb
@@ -1,5 +1,22 @@
 class MessagesController < ApplicationController
-    # RoomsChannel.broadcast_to(room, {
-    #     messages: room.messages
-    # })
+    before_action :authorize, only: [:index]
+    def index
+        room = Room.find(params[:room_id])
+        if room
+            if room.users.where(id: @user.id)
+                render json: room.messages
+            else
+                render json: {error: "401 Unauthorized", message:"You are not authorized to view this room"}
+            end
+        else
+            render json: {error: "404 not found", message: "Invalid Room uuid"}, status: :not_found
+        end
+    end
+
+    def create
+                # RoomsChannel.broadcast_to(room, {
+        #     messages: room.messages
+        # })
+        
+    end
 end
