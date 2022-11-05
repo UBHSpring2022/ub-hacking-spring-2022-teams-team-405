ae05c98d574b167fe065256b3a160a660600e2b9
Mustafa Binalhag <mbin828@gmail.com>
2022-11-05 16:03:45 -0400

    super tokens generator

---
+++ back-end/db/migrate/20221105195547_create_super_tokens.rb
@@ -0,0 +1,13 @@
+class CreateSuperTokens < ActiveRecord::Migration[7.0]
+  def change
+    create_table :super_tokens do |t|
+      t.string :token
+      t.datetime :expiry
+      t.integer :user_id
+      t.string :agent
+      t.string :client_ip
+
+      t.timestamps
+    end
+  end
+end
