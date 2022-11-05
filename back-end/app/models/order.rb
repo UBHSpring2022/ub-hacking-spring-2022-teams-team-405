492a41211941a3082e060b8886e6c0a9b28fdb24
Mohamed Muflahi <muflahimohamed88@gmail.com>
2022-11-05 16:47:48 -0400

    relationships

--- back-end/app/models/order.rb
+++ back-end/app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
-end
+    belongs_to :user
+    belongs_to :product
+end 
--- back-end/app/models/product.rb
+++ back-end/app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    has_many :orders
+    belongs_to :user
 end
