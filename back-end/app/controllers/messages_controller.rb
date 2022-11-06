37c2dc404c2d5d0c87821c29043a5979b5aa1913
Mohamed Muflahi <muflahimohamed88@gmail.com>
2022-11-06 04:55:01 -0500

    websockets

--- back-end/app/controllers/messages_controller.rb
+++ back-end/app/controllers/messages_controller.rb
@@ -15,7 +15,9 @@ class MessagesController < ApplicationController
         if is_authorized?(room)
             message = Message.create!(user_id: @user.id, room_id: room.id, content: params[:content])
             RoomsChannel.broadcast_to(room, {
-                messages: room.messages
+                # messages: ActiveModel::SerializableResource.new(room.messagesp)
+                # messages: MessageSerializer.new(message)
+                room:  RoomSerializer.new(room)
             })
         else
             render json: {error: "401 Unauthorized", message:"You are not authorized to view this room"},status: :unauthorized
