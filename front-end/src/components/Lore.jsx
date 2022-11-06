dc29ae5b4e1c2b7fa139bf4165e4b52fe7dce4f1
Mustafa Binalhag <mbin828@gmail.com>
2022-11-06 03:24:31 -0500

    favicon lore setup and footer

---
+++ front-end/src/components/Lore.jsx
@@ -0,0 +1,13 @@
+const Lore = () => {
+	return (
+		<div className="Lore">
+			<h1 id="Lore" className="Lore-title"> Lore </h1>
+			<p className="Lore-paragraph">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+				Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+				dolor in reprehenderit in voluptate velit esse cillu
+			</p>
+		</div>
+	);
+};
+export default Lore;
---
+++ front-end/src/pages/Market.jsx
@@ -0,0 +1,8 @@
+const Market = () => {
+  return (
+    <>
+      <h1> This is Market </h1>
+    </>
+  );
+}
+export default Market;
