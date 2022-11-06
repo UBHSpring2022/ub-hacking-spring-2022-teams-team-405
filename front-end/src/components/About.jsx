cd415db9f7cf520d542901884f67af497c00c1c0
Mustafa Binalhag <mbin828@gmail.com>
2022-11-05 21:42:02 -0400

    setting up front end folder/file structure

---
+++ front-end/src/components/About.jsx
@@ -0,0 +1,13 @@
+import "../styles/About.scss"
+const About = () => {
+  return (
+    <div className="About">
+      <h1 className="About-title"> This is About </h1>
+
+      <div className="About-second">
+        <h1 className="About-second-title">Second text</h1>
+      </div>
+    </div>
+  );
+}
+export default About;
---
+++ front-end/src/pages/Landing.jsx
@@ -0,0 +1,12 @@
+import About from "../components/About.jsx"
+const Landing = () => {
+  return (
+    <>
+        <About/>
+        {/* <Transcations/> name what ever u want */}
+        {/* name of component */}
+        {/* Footer */}
+    </>
+  );
+}
+export default Landing;
