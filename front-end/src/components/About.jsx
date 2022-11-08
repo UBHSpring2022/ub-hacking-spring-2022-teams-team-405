aa0fbf924e47f605605fcf49a2b21dd7ddc45c97
Mustafa Binalhag <mbin828@gmail.com>
2022-11-08 17:01:33 -0500

    detail qol improvments

--- front-end/src/components/About.jsx
+++ front-end/src/components/About.jsx
@@ -1,4 +1,6 @@
 import coinLogo from "../assets/mozam-logo.png"
+import Divider from '@mui/material/Divider';
+
 const About = () => {
   return (
     <div className="About">
@@ -6,12 +8,16 @@ const About = () => {
       <p>Mozam is a marketplace for sellers and buyers to exchange 
 rare fish with anonymity. Our mission is to serve our customers 
 with the best service possible while protecting their identity   </p>
+<Divider flexItem></Divider>
+
       <h1 className="About-title"> How we handle <span className="About-underlines">Transactions:</span></h1>
       <img src={coinLogo} alt="Coin" />
       <p>All transactions will be made via our latest cryptocurrency, Mozam 
 coin. With blockchain technology, we are able to insure that 
 all customers can buy and sell Rare fish with anonymity. 
 We want to ensure privacy for all customers! </p>
+<Divider flexItem></Divider>
+
     </div>
   );
 }
--- front-end/src/pages/Detail.jsx
+++ front-end/src/pages/Detail.jsx
@@ -1,206 +1,212 @@
 import "../styles/Detail.scss";
 import { useEffect, useState } from "react";
-import contract from './nft.json';
-import { ethers } from 'ethers';
+import contract from "./nft.json";
+import { ethers } from "ethers";
 import "../styles/MarketItem.scss";
-import coinLogo from "../assets/mozam-logo.png"
+import coinLogo from "../assets/mozam-logo.png";
+import Divider from '@mui/material/Divider';
 
 import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
 
 const contractAddress = "0x355638a4eCcb777794257f22f50c289d4189F245";
 const abi = contract.abi;
-const Detail = ({setOpenModal,setIsMining}) => {
-    const navigate = useNavigate();
-    const [product, setProduct] = useState({})
-    const [seller, setSeller] = useState({})
-    const [currentAccount, setCurrentAccount] = useState(null);
+const Detail = ({ setOpenModal, setIsMining }) => {
+	const navigate = useNavigate();
+	const [product, setProduct] = useState({});
+	const [seller, setSeller] = useState({});
+	const [currentAccount, setCurrentAccount] = useState(null);
 
-    useEffect(() => {
-      fetch(`http://localhost:3000/products/${window.location.href.match(/\d+$/)[0]}`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-        },
-      })
-      .then((response) => response.json())
-      .then((result) => {
-        // console.log("result", result);
-        if(result.error){
-            // FAILED
+	useEffect(() => {
+		fetch(`http://localhost:3000/products/${window.location.href.match(/\d+$/)[0]}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+			},
+		})
+			.then((response) => response.json())
+			.then((result) => {
+				// console.log("result", result);
+				if (result.error) {
+					// FAILED
 					Swal.fire(data["error"], data["message"], "error");
-        }else{
-            // success
-            setProduct(result)
-            console.log("result", result);
-            setSeller(result.user)
-        }
-        });
-    }, [])
-            
-  const checkWalletIsConnected = async () => {
-    const { ethereum } = window;
+				} else {
+					// success
+					setProduct(result);
+					console.log("result", result);
+					setSeller(result.user);
+				}
+			});
+	}, []);
 
-    if (!ethereum) {
-      console.log("Make sure you have Metamask installed!");
-      return;
-    } else {
-      console.log("Wallet exists! We're ready to go!")
-    }
+	const checkWalletIsConnected = async () => {
+		const { ethereum } = window;
 
-    const accounts = await ethereum.request({ method: 'eth_accounts' });
+		if (!ethereum) {
+			console.log("Make sure you have Metamask installed!");
+			return;
+		} else {
+			console.log("Wallet exists! We're ready to go!");
+		}
 
-    if (accounts.length !== 0) {
-      const account = accounts[0];
-      console.log("Found an authorized account: ", account);
-      setCurrentAccount(account);
-    } else {
-      console.log("No authorized account found");
-    }
-  }
+		const accounts = await ethereum.request({ method: "eth_accounts" });
 
-  const connectWalletHandler = async () => {
-    const { ethereum } = window;
+		if (accounts.length !== 0) {
+			const account = accounts[0];
+			console.log("Found an authorized account: ", account);
+			setCurrentAccount(account);
+		} else {
+			console.log("No authorized account found");
+		}
+	};
 
-    if (!ethereum) {
-      alert("Please install Metamask!");
-    }
+	const connectWalletHandler = async () => {
+		const { ethereum } = window;
 
-    try {
-      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-      console.log("Found an account! Address: ", accounts[0]);
-      setCurrentAccount(accounts[0]);
-    } catch (err) {
-      console.log(err)
-    }
-  }
+		if (!ethereum) {
+			alert("Please install Metamask!");
+		}
 
-  const mintNftHandler = async () => {
-    try {
-      const { ethereum } = window;
+		try {
+			const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+			console.log("Found an account! Address: ", accounts[0]);
+			setCurrentAccount(accounts[0]);
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-      if (ethereum) {
-        const provider = new ethers.providers.Web3Provider(ethereum);
-        const signer = provider.getSigner();
-        const nftContract = new ethers.Contract(contractAddress, abi, signer);
+	const mintNftHandler = async () => {
+		try {
+			const { ethereum } = window;
 
-        console.log("Initialize payment");
-        let nftTxn = await nftContract.mintNFTs(1, { value: ethers.utils.parseEther("0.000000001") });
+			if (ethereum) {
+				const provider = new ethers.providers.Web3Provider(ethereum);
+				const signer = provider.getSigner();
+				const nftContract = new ethers.Contract(contractAddress, abi, signer);
 
-        console.log("Mining... please wait");
-        setIsMining(true)
-        setOpenModal(true)
-        await nftTxn.wait();
-        setIsMining(false)
-        setOpenModal(false)
+				console.log("Initialize payment");
+				let nftTxn = await nftContract.mintNFTs(1, { value: ethers.utils.parseEther("0.000000001") });
 
-        Swal.fire(
-          'Mined Successfully 👷🏽!',
-          `See transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`,
-          'success'
-        )
-        let product_id = window.location.href.match(/\d+$/)[0]
-        fetch('http://localhost:3000/complete-order', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "super-token": localStorage.getItem("token")
-            },
-            body: JSON.stringify({
-                product_id
-            }),
-        })
-        .then((response) => response.json())
-        .then((result) => {
-            // console.log("result", result);
-            if(result.error){
-                // FAILED
-            }else{
-                // success
-                navigate('/market')
-                console.log("result", result);
-            }
-        });
+				console.log("Mining... please wait");
+				setIsMining(true);
+				setOpenModal(true);
+				await nftTxn.wait();
+				setIsMining(false);
+				setOpenModal(false);
 
-      } else {
-        console.log("Ethereum object does not exist");
-      }
+				Swal.fire("Minted Successfully 👷🏽!", `See transaction: https://goerli.etherscan.io/tx/${nftTxn.hash}`, "success");
+				let product_id = window.location.href.match(/\d+$/)[0];
+				fetch("http://localhost:3000/complete-order", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+						Accept: "application/json",
+						"super-token": localStorage.getItem("token"),
+					},
+					body: JSON.stringify({
+						product_id,
+					}),
+				})
+					.then((response) => response.json())
+					.then((result) => {
+						// console.log("result", result);
+						if (result.error) {
+							// FAILED
+						} else {
+							// success
+							navigate("/market");
+							console.log("result", result);
+						}
+					});
+			} else {
+				console.log("Ethereum object does not exist");
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	};
 
-    } catch (err) {
-      console.log(err);
-    }
-  }
+	const connectWalletButton = () => {
+		return (
+			<button onClick={connectWalletHandler} className="second">
+				Connect Wallet
+			</button>
+		);
+	};
 
-  const connectWalletButton = () => {
-    return (
-      <button onClick={connectWalletHandler} className="second">
-        Connect Wallet
-      </button>
-    )
-  }
+	const mintNftButton = () => {
+		return (
+			<button className="second" onClick={mintNftHandler}>
+				Buy
+			</button>
+		);
+	};
 
-  const mintNftButton = () => {
-    return (
-      <button className="second" onClick={mintNftHandler}>
-        Buy 
-      </button>
-    )
-  }
-
-  useEffect(() => {
-    checkWalletIsConnected();
-  }, [])
+	useEffect(() => {
+		checkWalletIsConnected();
+	}, []);
 
 	return (
 		<div className="detail">
+
 			<div className="detail-image">
-                <img src={product.image_url} alt="product" />
-                <div className="detail-image-child">
-                    <p>Description:</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fermentum volutpat velit sollicitudin aliquam. Proin turpis dolor, suscipit ut eros quis, commodo dictum urna. </p>
-                </div>
-            </div>
-            <div className="detail-details">
-                <div>
-                    <h1>{product.name}</h1>
-                    <p>{seller.display_name}</p>
-                    <div style={{justifyContent: "flex-start"}} className="MarketItem-price">
-					    <img src={coinLogo} alt="coin" style={{width: "7vh", height: "7vh"}}/>
-					    <p style={{fontSize: "larger", marginLeft: "20px"}} >
-						{product.price} <span>MOZ</span>
-					    </p>
-				    </div>
-                </div>
-                <button className="first" onClick={()=>{
-                    let product_id = window.location.href.match(/\d+$/)[0]
-                    fetch('http://localhost:3000/create-room', {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                            "Accept": "application/json",
-                            "super-token": localStorage.getItem("token")
-                        },
-                        body: JSON.stringify({
-                            product_uuid: product_id,
-                            seller_email: seller.email
-                        }),
-                    })
-                    .then((response) => response.json())
-                    .then((result) => {
-                        // console.log("result", result);
-                        if(result.error){
-                            // FAILED
-                            
-                        }else{
-                            // success
-                            navigate(`/chat/${result.id}`)
-                        }
-                    })
-                }}>Message Seller</button>
-                {currentAccount ? mintNftButton() : connectWalletButton()}
-            </div>
+				
+				{!product.image_url ?<img src={"https://static.dezeen.com/uploads/2019/02/new-zara-logo-hero-1.jpg"} alt={product.name} />:<img src={product.image_url} alt="product" />}
+				<div className="detail-image-child">
+					<p>Description:</p>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fermentum volutpat velit sollicitudin aliquam. Proin
+						turpis dolor, suscipit ut eros quis, commodo dictum urna.
+					</p>
+				</div>
+			</div>
+			<div className="detail-details">
+				<div className="detail-title">
+					<h1>{product.name}</h1>
+					<p>{seller.display_name}</p>
+					<div className="detail-price">
+						<img src={coinLogo} alt="coin" />
+						<p style={{ fontSize: "larger", marginLeft: "20px" }}>
+							{product.price} <span>MOZ</span>
+						</p>
+					</div>
+				</div>
+				{currentAccount ? mintNftButton() : connectWalletButton()}
+
+				<button
+					className="first"
+					onClick={() => {
+						let product_id = window.location.href.match(/\d+$/)[0];
+						fetch("http://localhost:3000/create-room", {
+							method: "POST",
+							headers: {
+								"Content-Type": "application/json",
+								Accept: "application/json",
+								"super-token": localStorage.getItem("token"),
+							},
+							body: JSON.stringify({
+								product_uuid: product_id,
+								seller_email: seller.email,
+							}),
+						})
+							.then((response) => response.json())
+							.then((result) => {
+								// console.log("result", result);
+								if (result.error) {
+									// FAILED
+									Swal.fire("400 Error", `Please login and try again, error detail : ${result.error} `, "error");
+								} else {
+									// success
+									navigate(`/chat/${result.id}`);
+								}
+							});
+					}}>
+					Message Seller
+				</button>
+			</div>
+
 		</div>
 	);
 };
